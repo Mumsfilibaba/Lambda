8a79d02e642614fe04125e0d2c06de5c493146ae
Alexander Dahlin <alexander@dahlins.nu>
2019-09-12 01:41:19 +0200

    Update Main.cpp

--- Sandbox/Main.cpp
+++ Sandbox/Main.cpp
@@ -7,12 +7,12 @@ int main()
 	DBG_MEMLEAK_CHECK();
 
 	EngineParams params = {};
-	params.pTitle = "Lambda Engine - Sandbox [Vulkan]";
-	params.GraphicsDeviceApi = GRAPHICS_API_VULKAN;
-	params.SampleCount = 1;
-	params.WindowWidth = 1920;
-	params.WindowHeight = 1080;
-	params.Fullscreen = false;
+	params.pTitle               = "Lambda Engine - Sandbox [Vulkan]";
+	params.GraphicsDeviceApi    = GRAPHICS_API_VULKAN;
+	params.SampleCount          = 1;
+	params.WindowWidth          = 1920;
+	params.WindowHeight         = 1080;
+	params.Fullscreen           = false;
 
 	Application* pApp = DBG_NEW SandBox(params);
 	int32 result = pApp->Run();
